e38064b2c830929a574b42b7231330822ac0656e
Rosie <dev.kayoung@gmail.com>
2023-09-12 14:27:31 +0900

    study: 리플렉션으로 특정 어노테이션을 가진 메서드 추출

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +11,14 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Arrays.stream(clazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> {
+                    try {
+                        method.invoke(clazz.newInstance());
+                    } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
